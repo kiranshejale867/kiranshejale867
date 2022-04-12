7c79182b7ed932b3dd8cfaa8fb0213e2c6fb1ef9
kiranshejale867 <103430315+kiranshejale867@users.noreply.github.com>
2022-04-12 17:25:11 +0530

    Add files via upload

---
+++ ForLoopCheck.java
@@ -0,0 +1,20 @@
+
+class ForLoopCheck
+{
+
+	public static void main(String[] args)
+	{
+
+	java.util.Scanner sc= new java.util.Scanner(System.in);
+	
+	System.out.println("Enter the range ..!");
+	int fn=sc.nextInt();
+	int ln=sc.nextInt();
+
+	for(int a=fn; a<=ln; a++)
+	{
+	System.out.println(a);
+	}
+
+	}
+}
---
+++ OddNumber.java
@@ -0,0 +1,19 @@
+// all the odd number between 10-20  , 123 -140
+
+// loop (Multiple iteration ) && conditional statement (if statement)
+
+
+class OddNumber
+{
+	public static void main(String[] args)
+	{
+		for(int i=123;i<=140;i++)
+		{
+			if(i%2==1)
+			{
+			System.out.println(i);
+			}
+		}
+	}
+}
+		
