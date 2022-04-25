78e082c410b258ec8545d2f80c748de12d510a53
kiranshejale867 <103430315+kiranshejale867@users.noreply.github.com>
2022-04-25 15:21:13 +0530

    Add files via upload

---
+++ Lab 2.java
@@ -0,0 +1,47 @@
+1.First program
+ 
+class Maxvotes
+{
+
+     public static void main(String[] args)
+     {
+       
+      int v1=1136;
+      int v2=7636;
+      int v3=11628;
+
+      int Totalvotes= 20400
+
+      int maxpercentage= v3/total votes* 100
+
+      System.out.println(maxpercentage)
+      }
+}
+
+2.Second program
+
+class Population
+{ 
+        public static void main(String[] args)
+        {
+         int p1;
+         int p2;
+         int pi;
+         int A;
+
+        //p1=population at start 
+        //p2=Total population increased after 10 years
+ 
+          //pi=exact population increased in 10 years=p2-p1 
+
+         A= Total percentage of population increased in 10 years=pi/p1*100;
+
+         B = percentage increased per year=A/10;
+
+         System.out.println(B + " is % of population increased per year")
+         }
+}
+
+3.error: illegal start of expression is output
+
+4. 32 output
