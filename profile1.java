4d2798c9c55307d22ad37969f881c7d67f6d0120
kiranshejale867 <103430315+kiranshejale867@users.noreply.github.com>
2022-06-06 16:41:20 +0530

    Add files via upload

---
+++ profile1.java
@@ -0,0 +1,135 @@
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="bootstrap-4.5.3-dist/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="profile.css">
+
+</head>
+
+<body> 
+    <div class="container">
+ <div class="main">
+
+<div class="topbar">
+     <a href="">logout</a>
+     <a href="">Support</a> 
+     <a href="">work</a>
+     <a href="">Home</a>
+</div>
+
+<div class="row">
+
+<div class="col-md-4 at-1"> <div class="card text-center sidebar">
+
+<div class="card-body">
+
+<!-- cing sec-image.jpg" class="rounded-circle doth "150"s <div class="st-3"> -->
+
+<h3>KIRAN SHEJALE</h3>
+
+<a href="">Home</a>
+
+<a href="">Work</a>
+
+<a href="">Support</a>
+
+<a href="">Setting</a>
+
+<a href="">Signout</a>
+
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-8 mt-1"> <div class="card mb-3 content">
+
+    <h1 class="m-3 pt-3">About</h1> <div class="card-body">
+    
+    <div class="row">
+    
+    <div class="col-md-3"> <h5>Full Name</h5>
+        KIRAN SHEJALE    
+    </div>
+    
+    <div class="col-md-9 text-secondary"> 
+    
+    </div>
+    
+    </div>
+    
+    <hr>
+    
+    <div class="row">
+    
+    <div class="col-md-3"> <h5>Email</h5>
+    
+    </div>
+    
+    <div class="col-md-9 text-secondary"> kiranshejale@gmail.com
+    
+    </div>
+    
+    </div>
+    
+    <hr>
+    
+    <div class="row"> <div class="col-md-3">
+    
+    <h5>Phone</h5>
+    </div>
+    <div class="col-md-9 text-secondary">
+         00923469874656
+
+
+
+</div>
+
+</div>
+
+<div class="row">
+
+<div class="col-md-3"> <h5>Address</h5>
+
+</div>
+
+<div class="col-md-9 text-secondary">
+
+street no. 4, xyz
+
+</div>
+
+</div>
+
+
+
+</div>
+
+</div>
+
+<div class="card mb-3 content">
+
+<h1 class="m-3">Recent Projects</h1>
+
+<div class="card-body"> <div class="row">
+
+<div class="col-md-3"> Project on Html</h5>
+
+</div>
+
+<div class="col-md-9 text-secondary">
+
+<!-- Project Description -->
+
+
+
+</div> 
+</div>
+</div> 
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
